5f4096c50997c9e30d5eaf660b48d016ce0076ed
Corsenac Pierre <pierre.corsenac@epitech.eu>
2019-11-14 12:16:44 -0500

    [FORMULA] [OUTSTANDING] : adding the outstanding formula

--- pymortgageformula.py
+++ pymortgageformula.py
@@ -7,6 +7,13 @@ def fixed_monthly_payments_formula(P, r, n):
     result = P * r * division
     return result
 
+def oustanding_loan_balance_formula(m, P, r, n):
+    top = ((1 + r)**n - (1 + r)**m)
+    bottom = (((1 + r)**n) - 1)
+    division = top / bottom
+    result = P * division
+    return result
+
 # get the principal of the loan
 def get_principal():
     P = input("What is the principal ?\n")
@@ -17,6 +24,17 @@ def get_principal():
 
     except ValueError:
         print("ERROR : not a correct Value\n")
+
+# get the outstanding month
+def get_outstanding_month():
+    m = input("What month do you want to check the Oustanding ?\n")
+
+    try:
+        m = int(m)
+        return m
+
+    except ValueError:
+        get_oustanding_month()
 
 # get the monthly interest rate of the loan
 def get_interest_rate():
@@ -46,13 +64,17 @@ def main():
     P = get_principal()
     r = get_interest_rate() / 100
     n = get_number_months()
+    m = get_outstanding_month()
 
     try:
-        result = fixed_monthly_payments_formula(P, r, n)
-        print("Fixed monthly payments = %s"% result)
+        monthly_payments = fixed_monthly_payments_formula(P, r, n)
+        oustanding = oustanding_loan_balance_formula(m, P, r, n)
+        print("Fixed monthly payments = %s"% monthly_payments)
+        print("Oustanding = %s"% oustanding)
+
     
     except:
-        print("ERROR while calculating the fixed_monthly_payments_formula")
+        print("ERROR while calculating the fixed_monthly_payments_formula or the oustanding")
         return
 
     
